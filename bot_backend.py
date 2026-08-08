7bd1ea7febd15662dde0ffc7bc467fc22623056f
Bhivanshu45 <bhivanshulawaniya6@gmail.com>
2026-08-08 19:16:07 +0530

    added streaming on AI response

--- bot_backend.py
+++ bot_backend.py
@@ -1,6 +1,6 @@
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
-from langchain_core.messages import BaseMessage
+from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_groq import ChatGroq
 from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.graph.message import add_messages
@@ -27,3 +27,7 @@ graph.add_edge(START, "chat_node")
 graph.add_edge("chat_node", END)
 
 chatbot = graph.compile(checkpointer=checkpointer)
+
+# chatbot.invoke
+
+# stream
